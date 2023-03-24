351058c9eba951eecaae08e51e59fca9a375bb48
Sameep1907 <w0822247@myscc.ca>
2023-03-24 16:34:58 -0400

    Created a new razor component and added it to the navigation bar

--- Swimming/obj/Debug/net5.0/RazorDeclaration/Pages/PoolNamesList.razor.g.cs
+++ Swimming/obj/Debug/net5.0/RazorDeclaration/Pages/PoolNamesList.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace Swimming.Shared
+namespace Swimming.Pages
 {
     #line hidden
     using System;
@@ -82,28 +82,14 @@ using Swimming.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/poolnameslist")]
+    public partial class PoolNamesList : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 33 "C:\Users\Megsoft\Desktop\Sameep\WEB315_Assignments_ThemeSwim\Swimming\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
